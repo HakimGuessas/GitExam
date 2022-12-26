e960020b4eeb303b7cd4807fe8888439e59433cd
Your Name <you@example.com>
2022-12-26 10:39:48 +0100

    m3

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-class Perssone{
+class Student{
     constructor(name,age){
         this.name=name;
         this.age=age;
     }
 }
-let s1 = new Perssone("hakim",19);
+let s1 = new Student("hakim",19);
 console.log(s1)
